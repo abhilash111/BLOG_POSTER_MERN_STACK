968a4ee9007e2e85dfee54b1f75842a56eb7443c
abhilash111 <abhilashkulkarni11@gmail.com>
2020-12-27 12:36:23 +0530

    Express App Setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('API is Running');
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Express app Running at Port ${PORT}`);
+})
